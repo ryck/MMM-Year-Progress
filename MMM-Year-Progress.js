c8924e63b290752f59a680d4fecf8ff820e0ba7c
Ricardo Gonzalez <rickgc@gmail.com>
2018-11-30 17:21:50 +0000

    👷 WIP

--- MMM-Year-Progress.js
+++ MMM-Year-Progress.js
@@ -28,11 +28,17 @@ Module.register("MMM-Year-Progress", {
 	},
 	// Override dom generator.
 	getDom: function() {
-		var wrapper = document.createElement("div");
+		let wrapper = document.createElement("div");
 		wrapper.className = "small align-right progress-bar"
 
 		// Year
-		var yearWrapper = document.createElement("div");
+		let yearWrapper = document.createElement("div");
+		let numWrapper = document.createElement("span");
+		numWrapper.className="numbers"
+		let barWrapper = document.createElement("span");
+		barWrapper.className = "bar"
+		let percentWrapper = document.createElement("span");
+		percentWrapper.className = "percent"
 		date = new Date()
 		const initialDate = new Date(date.getFullYear(), 0, 1)
 		const isLeapYear = year => {
@@ -42,7 +48,10 @@ Module.register("MMM-Year-Progress", {
 		const daysInYear = isLeapYear(date.getFullYear()) ? 366 : 365
 		const percentYear = Math.floor((dayYear / daysInYear) * 100)
 		const yearBar = this.progressBar(percentYear)
-		const yearDisplay = dayYear + "/" + daysInYear + " " + yearBar + " " + percentYear + "%"
+		numWrapper = dayYear + "/" + daysInYear
+		barWrapper = yearBar
+		percentWrapper = percentYear + "&%"
+		const yearDisplay = numWrapper + " " + barWrapper + " " + percentWrapper
 		yearWrapper.innerHTML = yearDisplay;
 
 		wrapper.appendChild(yearWrapper);
@@ -52,8 +61,9 @@ Module.register("MMM-Year-Progress", {
 		const daysInMonth = new Date(moment().year(), moment().month() + 1, 0).getDate();
 		const dayMonth = moment().date();
 		const percentMonth = Math.floor(dayMonth/daysInMonth*100);
-		const monthBar = this.progressBar(percentYear)
-		const monthDisplay = dayMonth + "/" + daysInMonth + " " + monthBar + " " + percentMonth + "%"
+		console.log('percentMonth', percentMonth);
+		const monthBar = this.progressBar(percentMonth)
+		const monthDisplay = dayMonth + "/" + daysInMonth + " " + monthBar + " <span class='percent'>" + percentMonth + "%</span>"
 		monthWrapper.innerHTML = monthDisplay
 
 		wrapper.appendChild(monthWrapper);
@@ -62,8 +72,9 @@ Module.register("MMM-Year-Progress", {
 		var weekWrapper = document.createElement("div");
 		const weekDay = moment().isoWeekday()
 		const percentWeek = Math.floor(weekDay/7*100);
+		console.log('percentWeek', percentWeek);
 		const weekBar = this.progressBar(percentWeek)
-		const weekDisplay = weekDay + "/7" + " " + weekBar + " " + percentWeek + "%"
+		const weekDisplay = weekDay + "/7" + " " + weekBar + " <span class='percent'>" + percentWeek + "%</span>"
 		weekWrapper.innerHTML = weekDisplay
 
 		wrapper.appendChild(weekWrapper);
@@ -72,9 +83,13 @@ Module.register("MMM-Year-Progress", {
 	},
 
 	progressBar: function(percent) {
+		console.log('percent', percent);
 		let progressBar = ""
 		for (let i = 5; i <= 100; i += 5) {
-			progressBar = (i < percent) ? progressBar + "▓" : progressBar + "░"
+			console.log('i', i);
+
+			progressBar = (i <= percent) ? progressBar + "▓" : progressBar + "░"
+			console.log('progressBar', progressBar);
 		}
 		return progressBar
 	}
